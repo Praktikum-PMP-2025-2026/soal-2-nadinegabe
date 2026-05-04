3ce5ff3b0d9db4aa70efd6f7eebb3958aaa32faf
nadinegabe <nadingabee@gmail.com>
2026-05-04 09:28:06 +0700

    Implement linked list merge and print functions

--- soal2.c
+++ soal2.c
@@ -1,5 +1,82 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
+
+typedef struct Node {
+    int data;
+    struct Node *next;
+} Node;
+
+Node* mergelist(Node* list1, Node* list2);
+void printlist(Node* head);
+Node* append(Node* head, int value);
+
+
+Node* mergelist(Node* list1, Node* list2) {
+    Node* merged = NULL;
+    Node** tail = &merged;
+
+    while (list1 && list2) {
+        if (list1->data < list2->data) {
+            *tail = list1;
+            list1 = list1->next;
+        } else {
+            *tail = list2;
+            list2 = list2->next;
+        }
+        tail = &((*tail)->next);
+    }
+
+    *tail = (list1 != NULL) ? list1 : list2;
+    return merged;
+}
+
+
+Node* append(Node* head, int value) {
+    Node* newNode = (Node*) malloc(sizeof(Node));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (head == NULL) return newNode;
+
+    Node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    return head;
+}
+
+
+void printlist(Node* head) {
+    if (head == NULL) {
+        printf("MERGED EMPTY\n");
+        return;
+    }
+
+    Node* ptr = head;
+    while (ptr != NULL) {
+        printf("%d ", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int N, M, i, x;
+    Node *list1 = NULL, *list2 = NULL, *list3 = NULL;
+    scanf("%d", &N);
+    for (i = 0; i < N; i++) {
+        scanf("%d", &x);
+        list1 = append(list1, x);
+    }
+    scanf("%d", &M);
+    for (i = 0; i < M; i++) {
+        scanf("%d", &x);
+        list2 = append(list2, x);
+    }
+    list3 = mergelist(list1, list2);
+    printlist(list3);
+
     return 0;
 }
